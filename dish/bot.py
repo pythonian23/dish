9079d955cdc30b6c9c8ae4daf815d94a2ee1b1c7
pythonaian23 <pythonaian23@gmail.com>
2022-11-08 23:57:22 +0900

    properly formatted output

--- dish/bot.py
+++ dish/bot.py
@@ -43,7 +43,7 @@ class Bot(discord.Client):
         if argv[0] in self.dishes.keys():
             argv = shlex.split(self.dishes[argv[0]]["run"]) + argv[1:]
             proc = subprocess.run(argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            await message.reply(proc.stdout.decode("utf-8"))
+            await message.reply(f"```ansi\n" + proc.stdout.decode("utf-8") + "\n```")
 
     def run(self):
         self.config.get("preinit", lambda _: None)(self)
