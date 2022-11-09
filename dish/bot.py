1fef61de7a540a9bea84e9566879851619bcefea
pythonaian23 <pythonaian23@gmail.com>
2022-11-10 01:05:05 +0900

    :sparkles: split & chain long messages
    double "msg = ..." due to bug in channel getting returning an Object

--- dish/bot.py
+++ dish/bot.py
@@ -14,10 +14,24 @@ async def _default_handler(_):
     return False
 
 
+async def respond(msg: discord.Message, content: str, client: discord.Client):
+    lines = content.splitlines(keepends=True)
+    buf = ""
+    for line in lines:
+        if len(buf) + len(line) + 12 > 2000:
+            msg = await msg.reply(f"```ansi\n{buf}\n```", mention_author=False)
+            msg = client.get_message(msg.id)
+            buf = ""
+        buf += line
+    if buf:
+        await msg.reply(f"```ansi\n{buf}\n```", mention_author=False)
+
+
 class Bot(discord.Client):
     def __init__(self, config: ConfigFile, **kwargs: typing.Any):
         super().__init__(
-            intents=discord.Intents(messages=True, message_content=True), **kwargs
+            intents=discord.Intents(messages=True, message_content=True),
+            **kwargs,
         )
         self.config: ConfigFile = config
         self.dishes: typing.Dict[str, Dish]
@@ -33,22 +47,23 @@ class Bot(discord.Client):
     async def on_ready(self):
         await self.config.get("postinit", _default_postinit)(self)
 
-    async def on_message(self, message: discord.Message):
-        if await self.config.get("handler", _default_handler)(message):
+    async def on_message(self, msg: discord.Message):
+        if await self.config.get("handler", _default_handler)(msg):
             return
-        if message.author == self.user:
+        if msg.author == self.user:
             return
 
-        argv = shlex.split(message.content)
+        argv = shlex.split(msg.content)
         if argv[0] in self.dishes.keys():
             argv = shlex.split(self.dishes[argv[0]]["run"]) + argv[1:]
             proc = subprocess.run(argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            await message.reply(
-                f"```ansi\n"
-                + proc.stdout.decode("utf-8")
-                + proc.stderr.decode("utf-8")
-                + "\n```"
-            )
+
+            out = proc.stdout.decode("utf-8")
+            err = proc.stderr.decode("utf-8")
+            if proc.returncode != 0:
+                await respond(msg, f"Error: {proc.returncode}\n\n{err}", self)
+            else:
+                await respond(msg, out, self)
 
     def run(self):
         self.config.get("preinit", lambda _: None)(self)
